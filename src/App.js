77867c9a24f4211be72550247081743b8a33a6d7
Cristina Quijano <cristina@cristinas-air-2.kpn>
2022-05-01 19:36:09 +0200

    lab

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { useEffect } from "react";
 
 function App() {
   return (
     <div className="App">
+      <div className="App">
+  <Navbar />
+
+  <div className="container">
+    <div className="row">
+      <CountriesList countries={countries} />,
+      
     </div>
-  );
+  </div>
+  </div>
+  </div>
+
+  useEffect (() => {
+  countries()
+}, []);
 }
 
 export default App;
