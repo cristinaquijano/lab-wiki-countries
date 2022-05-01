c0aec45f7d8364829246a996b23c47cb016c15f9
Cristina Quijano <cristina@cristinas-air-2.kpn>
2022-05-01 19:28:48 +0200

    Navbar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "context";
+import "./navbar.css";
+
+export function Navbar() {
+  const { user, logout } = useContext(AuthContext);
+  return (
+    <div className="mainNavbar">
+      <Link className="navLink" to="/">
+        <a className="navbarLinks">Wiki Countries</a>
+      </Link>
+
+      </div>
+  );
+}
